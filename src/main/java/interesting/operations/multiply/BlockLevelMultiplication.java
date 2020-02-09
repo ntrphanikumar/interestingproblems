03beafd745f98b6b4e2a49c6dcfa64f8bbb830c1
N T R Phani Kumar <ntrphanikumar@gmail.com>
2020-02-09 07:52:21 +0530

    blocks to primitive long[] for better performance

--- src/main/java/interesting/operations/multiply/BlockLevelMultiplication.java
+++ src/main/java/interesting/operations/multiply/BlockLevelMultiplication.java
@@ -4,90 +4,74 @@ import interesting.operations.NumberUtils;
 
 public class BlockLevelMultiplication {
 
-    private static final int MAX_SAFE_LENGTH = 19;
-
-    private static class BlockMultiplication {
-
-        private final int blockSize;
-        private final long pow10BlockSize;
-        private final String blockSizeZerosStr, multiplier, multiplicand;
-
-        private BlockMultiplication(String multiplier, String multiplicand) {
-            this.multiplier = multiplier;
-            this.multiplicand = multiplicand;
-            blockSize = determineBlockSize(multiplier, multiplicand);
-            pow10BlockSize = (long) Math.pow(10d, (double) blockSize);
-            blockSizeZerosStr = String.valueOf(pow10BlockSize).substring(1);
-        }
-
-        public String perform() {
-            return sum(powerMap(splitToBlocks(multiplier), splitToBlocks(multiplicand)));
-        }
-
-        private long[] powerMap(Long[] multiplierBlocks, Long[] multiplicandBlocks) {
-            long start = System.nanoTime();
-            int totalBlocksLength = multiplierBlocks.length + multiplicandBlocks.length;
-            long[] values = new long[totalBlocksLength - 1];
-            for (int i = 0; i < multiplierBlocks.length; i++) {
-                for (int j = 0; j < multiplicandBlocks.length; j++) {
-                    values[totalBlocksLength - i - j - 2] += multiplicandBlocks[j] * multiplierBlocks[i];
-                }
-            }
-            long end = System.nanoTime();
-            System.out.println("Power list generation took: " + (end - start));
-            return values;
-        }
-
-        private String sum(long[] powerList) {
-            long start = System.nanoTime();
-            String[] values = new String[powerList.length];
-            long carry = 0;
-            int maxPow = powerList.length - 1;
-            for (int pow = 0; pow < powerList.length; pow++) {
-                if (pow == maxPow) {
-                    long sum = powerList[pow] + carry;
-                    values[maxPow - pow] = sum == 0 ? "" : String.valueOf(sum);
-                    break;
-                }
-                long sum = carry + (powerList[pow] % pow10BlockSize);
-                carry = (powerList[pow] / pow10BlockSize) + (sum / pow10BlockSize);
-                String value = String.valueOf(sum % pow10BlockSize);
-                if (pow < maxPow && value.length() < blockSize) {
-                    value = blockSizeZerosStr.substring(value.length()) + value;
-                }
-                values[maxPow - pow] = value;
-            }
-
-            String result = String.join("", values);
-            long end = System.nanoTime();
-            System.out.println("Sum took: " + (end - start));
-            return result;
-        }
-
-        private Long[] splitToBlocks(String number) {
-            long start = System.nanoTime();
-            int idxAdj = number.length() % blockSize > 0 ? 0 : 1;
-            Long[] blocksArray = new Long[1 - idxAdj + number.length() / blockSize];
-            for (int idx = number.length(); idx > 0; idx -= blockSize) {
-                int beginIndex = idx - blockSize;
-                blocksArray[(idx / blockSize) - idxAdj] = Long
-                        .parseLong(number.substring(beginIndex < 0 ? 0 : beginIndex, idx));
-            }
-            long end = System.nanoTime();
-            System.out.println("Block generation took: " + (end - start));
-            return blocksArray;
-        }
-
-        private int determineBlockSize(String multiplier, String multiplicand) {
-            return 7;
-        }
-    }
-
     public String compute(String multiplier, String multiplicand) {
-        if (multiplier.length() + multiplicand.length() <= MAX_SAFE_LENGTH) {
+        final int longRange = (Long.MAX_VALUE + "").length() - 1;
+        if (multiplier.length() + multiplicand.length() <= longRange) {
             return String.valueOf(Long.parseLong(multiplier) * Long.parseLong(multiplicand));
         }
-        return new BlockMultiplication(multiplier, multiplicand).perform();
+        int blockSize = determineBlockSize(multiplier, multiplicand);
+        return sum(powerMap(splitToBlocks(multiplier, blockSize), splitToBlocks(multiplicand, blockSize)), blockSize);
+    }
+
+    private long[] powerMap(long[] multiplier, long[] multiplicand) {
+        long start = System.nanoTime();
+        int totalBlocksLength = multiplier.length + multiplicand.length;
+        long[] values = new long[totalBlocksLength - 1];
+        for (int i = 0; i < multiplier.length; i++) {
+            for (int j = 0; j < multiplicand.length; j++) {
+                values[totalBlocksLength - i - j - 2] += multiplicand[j] * multiplier[i];
+            }
+        }
+        long end = System.nanoTime();
+        System.out.println("Power list generation took: " + (end - start));
+        return values;
+    }
+
+    private String sum(long[] numbers, int blockSize) {
+        long start = System.nanoTime();
+        final long pow10BlockSize = (long) Math.pow(10d, (double) blockSize);
+        final String blockSizeZerosStr = String.valueOf(pow10BlockSize).substring(1);
+        String[] values = new String[numbers.length];
+        long carry = 0;
+        int maxPow = numbers.length - 1;
+        String emptyStr = "";
+        for (int pow = 0; pow < numbers.length; pow++) {
+            if (pow == maxPow) {
+                long sum = numbers[pow] + carry;
+                values[maxPow - pow] = sum == 0 ? emptyStr : String.valueOf(sum);
+                break;
+            }
+            long sum = carry + (numbers[pow] % pow10BlockSize);
+            carry = (numbers[pow] / pow10BlockSize) + (sum / pow10BlockSize);
+            String value = String.valueOf(sum % pow10BlockSize);
+            if (pow < maxPow && value.length() < blockSize) {
+                value = blockSizeZerosStr.substring(value.length()) + value;
+            }
+            values[maxPow - pow] = value;
+        }
+
+        String result = String.join("", values);
+        long end = System.nanoTime();
+        System.out.println("Sum took: " + (end - start));
+        return result;
+    }
+
+    private long[] splitToBlocks(String number, int blockSize) {
+        long start = System.nanoTime();
+        int idxAdj = number.length() % blockSize > 0 ? 0 : 1;
+        long[] blocksArray = new long[1 - idxAdj + number.length() / blockSize];
+        for (int idx = number.length(); idx > 0; idx -= blockSize) {
+            int beginIndex = idx - blockSize;
+            blocksArray[(idx / blockSize) - idxAdj] = Long
+                    .parseLong(number.substring(beginIndex < 0 ? 0 : beginIndex, idx));
+        }
+        long end = System.nanoTime();
+        System.out.println("Block generation took: " + (end - start));
+        return blocksArray;
+    }
+
+    private int determineBlockSize(String multiplier, String multiplicand) {
+        return 7;
     }
 
     public static void main(String[] args) {
